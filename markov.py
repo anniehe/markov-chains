2e291acf24da1c664e1b64b77134d23d8e610f54
Hackbright Student <info@hackbrightacademy.com>
2016-01-13 17:44:34 -0800

    cleaned up working code

--- markov.py
+++ markov.py
@@ -17,7 +17,7 @@ def open_and_read_file(file_path):
 
 
 def make_chains(text_string, n):
-    """Takes input text as string; returns _dictionary_ of markov chains.
+    """Takes input text as string; returns _dictionary_ of markov chains with n-gram keys.
 
     A chain will be a key that consists of a tuple of (word1, word2)
     and the value would be a list of the word(s) that follow those two
@@ -46,23 +46,28 @@ def make_chains(text_string, n):
         else:
             chains[word_gram].append(words[index + n])
 
-    # print chains
-
     return chains
 
 
 def make_text(chains, n):
-    """Takes dictionary of markov chains; returns random text."""
+    """Takes dictionary of markov chains with n-gram keys; returns random text."""
     
     while True:
+        # We're choosing a random tuple key from our dictionary, chains.
+        # If the first word in the random tuple key is capitalized, go forward.
         link = choice(chains.keys())
         if link[0].istitle():
             break
 
+    # Converting our tuple key to a list so we can join the words in our resulting text.
     link_list = list(link)
 
-    print "link list is:", link_list
-    
+    # While our tuple key is found in our chains dictionary,
+    # Choose a random word from the list of values found at that key.
+    # Add that random word to our list of words.
+    # We convert the tuple key slice from index 1 to the end into a list,
+    # And add the random word to create a new key link, which is converted back to a tuple.
+    # Finally, we combine all words in our list to make a long string (Markov text!)
     while link in chains:
         random_word = choice(chains[link])
         link_list.append(random_word)
@@ -70,14 +75,8 @@ def make_text(chains, n):
         link_temp = list(link[1:])
         link_temp.append(random_word)
         link = tuple(link_temp)
+        
         text = " ".join(link_list)
-    # print "link is:", link
-    # print "link list is:", link_list
-    # print "text is:", text
-
-
-    # print "text is:",text
-
 
     return text
 
